d12e44329c9cffb4b570ba38c0c655041dfb7132
Nikhil Yadav <nikhilyadav200530@gmail.com>
2026-01-02 15:30:27 +0530

    added proptypes and default prop val

--- src/components/Movie-card.jsx
+++ src/components/Movie-card.jsx
@@ -10,6 +10,20 @@ return(
     <h1>Duration:{props.duration}</h1>
     </div>
 )
+Moviecard.propTypes={
+    name:PropTypes.string.isRequired,   
+    release_date:PropTypes.number.isRequired,
+    rating:PropTypes.number.isRequired,
+    duration:PropTypes.string.isRequired    
+}
+Moviecard.defaultProps={
+    name:"Unknown",
+    release_date:2024,
+    rating:0,
+    duration:"0 minutes"
+}
+
+
 }
 
 export default Moviecard;
