ba86c12a724c276c53276a97cb71fdff019b217f
Nikhil Yadav <nikhilyadav200530@gmail.com>
2026-01-06 19:42:16 +0530

    rewrote for self assurity

--- src/components/state.jsx
+++ src/components/state.jsx
@@ -1,15 +1,33 @@
-import { useState } from "react";  
+// import { useState } from "react";  
 
 
 
-function FavouriteColor()
-    {
-        const [color, setColor] = useState("red")
-        return(
-            <>
-            <h1 style={{color: color}} >My fav color is {color}</h1>
-            </>
-        )
-    }
+// function FavouriteColor()
+//     {
+//         const [color, setColor] = useState("red")
+//         return(
+//             <>
+//             <h1 style={{color: color}} >My fav color is {color}</h1>
+//             </>
+//         )
+//     }
+
+// export default FavouriteColor
+
+
+
+
+
+import { useState } from "react";
+
+function FavouriteColor(){
+    
+    const [color,setColor]=useState("pink")
+    return(
+        <>
+        <h1 style={{color:color}}>The current color is {color}</h1>
+        </>
+    )
+}
 
 export default FavouriteColor
